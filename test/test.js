9df62e7980ae3ca6b5c02165e13497d4997ce7f9
Manuel Fernandez <manferlo81@gmail.com>
2019-04-16 18:11:12 +0700

    updated tests

--- test/test.js
+++ test/test.js
@@ -1,33 +1,57 @@
-const { applyParam, applyParamArgs, performParamApplier } = require("..");
+// @ts-check
+const { applyParam, applyParamArgs, applyMultiParamArgs, performParamApplier } = require("..");
 
-test("should apply perform param and return target", () => {
+describe("perform param applier", () => {
 
-  const target = {};
+  test("should apply perform param and return target", () => {
 
-  const performHandler = jest.fn();
+    const target = {};
+    const performHandler = jest.fn();
 
-  const result = applyParam(target, performHandler, [
-    performParamApplier,
-  ]);
+    const result = applyParam(target, performHandler, [
+      performParamApplier,
+    ]);
 
-  expect(performHandler).toHaveBeenCalledTimes(1);
-  expect(performHandler).toHaveBeenCalledWith(target);
-  expect(result).toBe(target);
+    expect(performHandler).toHaveBeenCalledTimes(1);
+    expect(performHandler).toHaveBeenCalledWith(target);
+
+    expect(result).toBe(target);
+
+  });
+
+  test("should apply multiple perform params and return target", () => {
+
+    const target = {};
+    const performHandler = jest.fn();
+    const args = [performHandler, performHandler];
+
+    const result = applyParamArgs(target, [
+      performParamApplier,
+    ], args, 0);
+
+    expect(performHandler).toHaveBeenCalledTimes(2);
+    expect(performHandler).toHaveBeenCalledWith(target);
+
+    expect(result).toBe(target);
+
+  });
+
+  test("should apply multiple perform params and return multiple targets", () => {
+
+    const targets = [{}, {}];
+    const performHandler = jest.fn();
+    const args = [performHandler, performHandler];
+
+    const result = applyMultiParamArgs(targets, [
+      performParamApplier,
+    ], args, 0);
+
+    expect(performHandler).toHaveBeenCalledTimes(4);
+    expect(performHandler).toHaveBeenCalledWith(targets[0]);
+    expect(performHandler).toHaveBeenCalledWith(targets[1]);
+
+    expect(result).toBe(targets);
+
+  });
 
 });
-
-test("should apply multiple perform params and return target", () => {
-
-  const target = {};
-  const performHandler = jest.fn();
-
-  const result = applyParamArgs(target, [
-    performParamApplier,
-  ], [performHandler, performHandler], 0);
-
-  expect(performHandler).toHaveBeenCalledTimes(2);
-  expect(performHandler).toHaveBeenCalledWith(target);
-  expect(result).toBe(target);
-
-});
-
