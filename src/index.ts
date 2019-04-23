98fe62a13edfd7384022116ee8891c82bdcba56a
Manuel Fernandez <manferlo81@gmail.com>
2019-04-23 16:56:38 +0700

    export everithing

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ export { default as eachProp } from "./each-prop";
 
 // Appliers
 export { default as createDBOptionApplier } from "./appliers/opt-create-db";
-// export { default as eventDBOptionApplier } from "./appliers/opt-event-db";
+export { default as eventDBOptionApplier } from "./appliers/opt-event-db";
 export { default as eventOnOptionApplier } from "./appliers/opt-event-on";
 export { default as objectDBOptionApplier } from "./appliers/opt-object-db";
 export { default as propOptionApplier } from "./appliers/opt-prop";
 export { default as createOptionParamApplier } from "./appliers/param-create-opt";
-// export { default as performParamApplier } from "./appliers/param-perf";
+export { default as performParamApplier } from "./appliers/param-perf";
 
 // Apply
 export {
