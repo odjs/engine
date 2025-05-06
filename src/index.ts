13418984b65ca83fe09f21507426d9a3e1fa57c8
Manuel Fernández <manferlo81@gmail.com>
2025-05-06 10:09:10 +0700

    chore: Modernize tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,35 @@
 // Type Check
-export { isNull, isObject, isString, isNumber, isCallable } from './type-check';
+export { isCallable, isNull, isNumber, isObject, isString } from './type-check';
 
 // Tools
-export { eachProp, EachPropCallback, EachPropCallbackParam } from './each-prop';
+export { eachProp } from './each-prop';
+export type { EachPropCallback, EachPropCallbackParam } from './each-prop';
 
-export { setPropHelper, addEventHelper, remEventHelper } from './helpers';
+export { addEventHelper, remEventHelper, setPropHelper } from './helpers';
 
 // Appliers
 export { default as createDBOptionApplier } from './appliers/opt-create-db';
 export { default as eventDBOptionApplier } from './appliers/opt-event-db';
-export { default as eventOnOptionApplier } from './appliers/opt-on-event';
 export { default as objectDBOptionApplier } from './appliers/opt-object-db';
+export { default as eventOnOptionApplier } from './appliers/opt-on-event';
 export { default as propOptionApplier } from './appliers/opt-prop';
 export { default as createOptionParamApplier } from './appliers/param-create-opt';
 export { default as performParamApplier } from './appliers/param-perform';
 
-export { EventOptionMap, eventOptionDB } from './dbs/db-event';
-export { ObjectOptionMap, objectOptionDB } from './dbs/db-object';
+export { eventOptionDB } from './dbs/db-event';
+export type { EventOptionMap } from './dbs/db-event';
+export { objectOptionDB } from './dbs/db-object';
+export type { ObjectOptionMap } from './dbs/db-object';
 
 // Apply
-export {
-  applyParam,
-  applyParamArgs,
-  applyMultiParamArgs,
-  applyPerformOptionParamArgs as applyPerforOptionParamArgs,
-  applyPerformOptionParamArgs,
-  applyMultiPerformOptionParamArgs as applyMultiPerforOptionParamArgs,
-  applyMultiPerformOptionParamArgs,
-} from './apply-params';
 export {
   applyOption,
   applyOptionObject,
 } from './apply-options';
+export {
+  applyMultiParamArgs, applyMultiPerformOptionParamArgs, applyMultiPerformOptionParamArgs as applyMultiPerforOptionParamArgs, applyParam,
+  applyParamArgs, applyPerformOptionParamArgs, applyPerformOptionParamArgs as applyPerforOptionParamArgs,
+} from './apply-params';
 
 // Types
 export * from './types';
